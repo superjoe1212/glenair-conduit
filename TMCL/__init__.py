e627134eb76770fb007a702128e89582bf13c256
Kylie Fernandez <52670103+kyliefern@users.noreply.github.com>
2019-09-04 09:04:42 -0700

    Delete __init__.py

--- TMCL/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bus import Bus
-from .motor import Motor
-from .commands import Command
-from .reply import Reply
-
-def connect ( serial_port, CAN = False ):
-    return Bus(serial_port, CAN)
